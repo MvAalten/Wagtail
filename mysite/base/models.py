92e4a758cac8a987f0ce4b1cbeee9460d3dc9d9f
MvAalten <157478216+MvAalten@users.noreply.github.com>
2026-02-05 11:27:50 +0100

    Finished 6th step
    nex step: https://docs.wagtail.org/en/stable/tutorial/create_portfolio_page.html

--- mysite/base/models.py
+++ mysite/base/models.py
@@ -1,13 +1,16 @@
 from django.db import models
+
+from modelcluster.fields import ParentalKey
+
 from wagtail.admin.panels import (
     FieldPanel,
+    FieldRowPanel,
+    InlinePanel,
     MultiFieldPanel,
-
     PublishingPanel,
 )
 
 from wagtail.fields import RichTextField
-
 from wagtail.models import (
     DraftStateMixin,
     PreviewableMixin,
@@ -15,14 +18,15 @@ from wagtail.models import (
     TranslatableMixin,
 )
 
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
+
+from wagtail.contrib.forms.panels import FormSubmissionsPanel
 from wagtail.contrib.settings.models import (
     BaseGenericSetting,
     register_setting,
 )
-
 from wagtail.snippets.models import register_snippet
 
-# ...keep the definition of the NavigationSettings model and add the FooterText model:
 @register_snippet
 class FooterText(
     DraftStateMixin,
@@ -68,3 +72,24 @@ class NavigationSettings(BaseGenericSetting):
             "Social settings",
         )
     ]
+
+    class FormField(AbstractFormField):
+        page = ParentalKey('FormPage', on_delete=models.CASCADE, related_name='form_fields')
+
+    class FormPage(AbstractEmailForm):
+        intro = RichTextField(blank=True)
+        thank_you_text = RichTextField(blank=True)
+
+        content_panels = AbstractEmailForm.content_panels + [
+            FormSubmissionsPanel(),
+            FieldPanel('intro'),
+            InlinePanel('form_fields'),
+            FieldPanel('thank_you_text'),
+            MultiFieldPanel([
+                FieldRowPanel([
+                    FieldPanel('from_address'),
+                    FieldPanel('to_address'),
+                ]),
+                FieldPanel('subject'),
+            ], "Email"),
+        ]
